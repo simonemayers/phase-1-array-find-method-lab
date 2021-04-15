eeacef164fe506ee036307139abe4b9926292604
simonemayers <simone21295@gmail.com>
2021-04-15 00:33:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+// function win(record) {
+//     if(record.result === "W"){
+//         return record.year
+//     }
+// }
+// function superbowlWin(record){
+//     return record.find(win)
+// }
 
+function superbowlWin(record){
+    let winningRecord; 
+    record.find(el => el.result === "W" ? winningRecord = el.year : winningRecord = undefined); 
+    return winningRecord
+}
